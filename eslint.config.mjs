e809015ced781ee2f6aa4d914770b0f1b016b906
ice breaker <1324318532@qq.com>
2024-07-06 20:44:40 +0800

    chore: add misc files

---
+++ eslint.config.mjs
@@ -0,0 +1,16 @@
+import { icebreaker } from '@icebreakers/eslint-config'
+
+export default icebreaker({}, {
+  languageOptions: {
+    globals: {
+      wx: true,
+      App: true,
+      Page: true,
+      getCurrentPages: true,
+      getApp: true,
+      Component: true,
+      requirePlugin: true,
+      requireMiniProgram: true,
+    },
+  },
+})
