e412b2c52c8e6ed5f92f0f9515580017e380f539
ice breaker <1324318532@qq.com>
2024-07-07 13:58:37 +0800

    feat: commit sass

--- weapp-tw.config.js
+++ weapp-tw.config.js
@@ -1,6 +1,9 @@
 /** @type {import('@weapp-tailwindcss/cli').UserConfig} */
 const config = {
   src: './miniprogram',
+  preprocessorOptions: {
+    sass: true,
+  },
   weappTailwindcssOptions: {
     rem2rpx: true,
   },
