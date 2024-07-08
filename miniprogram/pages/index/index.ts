e237cab38c3d3f856a6c6e7961e701613d40cdc8
ice breaker <1324318532@qq.com>
2024-07-09 00:18:35 +0800

    feat: upgarde native template

--- miniprogram/pages/index/index.ts
+++ miniprogram/pages/index/index.ts
@@ -12,8 +12,10 @@ Component({
     canIUseNicknameComp: wx.canIUse('input.type.nickname'),
   },
   methods: {
-    copy(xx){
-      console.log(xx)
+    copy(event: WechatMiniprogram.BaseEvent) {
+      wx.setClipboardData({
+        data: event.target.dataset.copyUrl
+      })
     },
     getUserProfile() {
       // 推荐使用wx.getUserProfile获取用户信息，开发者每次通过该接口获取用户个人信息均需用户确认，开发者妥善保管用户快速填写的头像昵称，避免重复弹窗
--- weapp-tw.config.js
+++ weapp-tw.config.js
@@ -2,11 +2,21 @@
 const config = {
   src: './miniprogram',
   preprocessorOptions: {
+    // 你可以开启这个选项来打开主动 sass 编译
+    // 这可以让你有能力，添加一些额外的 postcss 插件，不打开可能会报错
     sass: true,
   },
   weappTailwindcssOptions: {
     rem2rpx: true,
   },
+  // 你可以开启这个选项来让 cli 主动编译 typescript
+  // 需要先安装 typescript 再开启
+  // typescriptOptions: true,
+
+  // 开启后就不需要在 project.config.json 声明
+  // "useCompilerPlugins": [
+  //   "typescript"
+  // ],
 }
 
 module.exports = config
